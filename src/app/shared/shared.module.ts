56f98ac460682282ae5415adc59684067d189556
Michael Lean Pedrasoli <pedrasolimichael@gmail.com>
2019-08-06 23:21:04 -0300

    Improving BreadCrumbComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { RouterModule } from '@angular/router';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 
@@ -11,11 +13,13 @@ import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.compon
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    RouterModule
   ],
   exports: [
     // Modules
     CommonModule,
     ReactiveFormsModule,
+    RouterModule,
 
     // Components
     BreadCrumbComponent
