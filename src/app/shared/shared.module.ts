ab38226774f76caa04e83a7d1a5981107abf4e96
Michael Lean Pedrasoli <pedrasolimichael@gmail.com>
2019-08-07 00:37:59 -0300

    Adding control ServerErrorMessage

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,12 +7,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
+import { ServerErrorMessagesComponent } from './components/server-error-messages/server-error-messages.component';
 
 @NgModule({
   declarations: [
     BreadCrumbComponent,
     PageHeaderComponent,
-    FormFieldErrorComponent
+    FormFieldErrorComponent,
+    ServerErrorMessagesComponent
   ],
   imports: [
     CommonModule,
@@ -28,7 +30,8 @@ import { FormFieldErrorComponent } from './components/form-field-error/form-fiel
     // Components
     BreadCrumbComponent,
     PageHeaderComponent,
-    FormFieldErrorComponent
+    FormFieldErrorComponent,
+    ServerErrorMessagesComponent
   ]
 })
 export class SharedModule { }
